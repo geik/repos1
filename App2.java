73ae65267d36cce81017ccd486eb98e449625d73
Guus Eikens <guuseikens@deltalloyd.nl>
2016-10-04 05:03:28 +0200

    yes

---
+++ App2.java
@@ -0,0 +1,13 @@
+package com.guus.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyApp2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from App2!" );
+    }
+}
