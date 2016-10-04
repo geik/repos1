632358bc6e287a8f33808ea907cb128c29547bf2
Guus Eikens <guuseikens@deltalloyd.nl>
2016-10-04 04:47:34 +0200

    toe maar

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.guus.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World again from github!" );
+    }
+}
